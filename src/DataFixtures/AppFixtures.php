6a435afbad9a4b5158204f41828357e10677fa81
Layannd <brth.joana@gmail.com>
2024-10-10 11:00:31 +0200

    update code

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setNom('Bureth')->setPrenom(prenom: 'Joana');
+        $user->setEmail(email:'brth.joana@gmail.com');
+
+        $encoded = $this->encoder->hashPassword($user, '123');
+        $user->setPassword($encoded);
+        $user->setRoles(['ROLE_USER']);
+
+        $manager->persist($user);
 
         $manager->flush();
     }
